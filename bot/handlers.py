b772a82b5d65b6f40c2b07595bd4b4de9b51fd09
hiimluck3r <luck3rinc@yandex.ru>
2023-08-26 20:26:36 +0300

    add main menu + connection to db

--- bot/handlers.py
+++ bot/handlers.py
@@ -6,6 +6,7 @@ import aiogram.utils.markdown as fmt
 from bot.dispatcher import dp, bot
 from bot.keyboards import *
 from aiogram import types
+from time import sleep
 from bot import DB, USER, PASSWORD, HOST, ROOT, PORT
 from aiogram.utils.exceptions import BotBlocked
 from aiogram.dispatcher import FSMContext
@@ -15,4 +16,35 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 with open('bot/admins.json', 'r') as admin_file:
     admins = json.loads(json.load(admin_file))['admins']
 
-print(admins)
+"""
+Подключение к БД
+"""
+while True:
+    try:
+        conn = psycopg2.connect(dbname=DB, user=USER, password=PASSWORD, host=HOST, port=PORT) #указывать в .env
+        print('Connection to database is established')
+        break
+        
+    except Exception as e:
+        print("Can't establish connection to database. Error:", e)
+        sleep(3)
+
+"""
+/start
+"""
+
+@dp.message_handler(commands="start")
+async def greeter(message: types.Message):
+    greet = "Бот для инвентаризации."
+
+    keyboard = goto_menu()
+    await message.answer(greet, reply_markup=keyboard)
+
+"""
+Главное меню
+"""
+@dp.message_handler(Text(equals="Главное меню"))
+async def main_menu(message: types.Message):
+    keyboard = get_main_menu()
+    await message.answer(f"Главное меню", reply_markup=keyboard)
+
