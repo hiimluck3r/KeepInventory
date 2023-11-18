16fdf4ac61092c51c5f681eb6d8bab5cd40a324b
hiimluck3r <luck3rinc@yandex.ru>
2023-11-18 13:01:53 +0300

    fix: :bug: fixed a bug when qr code could have been scanned

--- app/barcodes/barcode_reader.py
+++ app/barcodes/barcode_reader.py
@@ -4,9 +4,14 @@ from pyzbar import pyzbar
 async def get_code(image):
     try:
         image = cv2.imread(image)
-        barcode = pyzbar.decode(image)
-        bdata = barcode[0].data.decode("utf-8")
-        return [True, int(bdata)]
+        barcodes = pyzbar.decode(image)
+        for barcode in barcodes:
+            btype = barcode.type
+            if btype == 'QRCODE':
+                pass
+            else:
+                bdata = barcode.data.decode("utf-8")
+                return [True, bdata]
         
     except Exception as e:
         return [False, e]
